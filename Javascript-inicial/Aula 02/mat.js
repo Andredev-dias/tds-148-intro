143aa34100dcb5991df0a2fc073900d674d7896f
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-19 16:17:05 -0300

    media

--- Javascript-inicial/Aula 02/mat.js
+++ Javascript-inicial/Aula 02/mat.js
@@ -35,4 +35,22 @@ function div(){
     } 
 }
 
-
+function media(){
+    var m1 = parseFloat(document.getElementById("m1").value)
+    var m2 = parseFloat(document.getElementById("m2").value) 
+    var m3 = parseFloat(document.getElementById("m3").value) 
+    var m4 = parseFloat(document.getElementById("m4").value) 
+    var m5 = parseFloat(document.getElementById("m5").value) 
+    var media = document.getElementById("respMedia")
+    var msg = document.getElementById("msg")
+    var res = (m1 + m2 + m3 + m4 + m5) / 5
+    if(res >= 7){
+        media.textContent = res
+        msg.textContent = "PASSOU"
+        msg.style.color = "green"
+    }else{
+        media.textContent = res
+        msg.textContent = "REPROVOU"
+        msg.style.color = "red"
+    }
+}
